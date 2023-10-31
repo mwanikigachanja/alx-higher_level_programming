ec05ddfd67f6aa1462d13eae26f2deebefde7f70
Mwaniki Charles <mwanikigachanja@gmail.com>
2023-10-31 10:00:25 -0700

    prints a string in uppercase followed by a new line.

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
-islower = __import__('7-islower').islower
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
+#!/usr/bin/python3
+def uppercase(str):
+    for word in str:
+        if (ord(word) > 96 and ord(word) <= 124):
+            word = chr(ord(word) - 32)
+        print("{}".format(word), end='')
+    print()
